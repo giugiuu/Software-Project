1b8f4ff748ba12801f895bfea4c1342412a0b9f7
amr <amr.hedeiwy@gmail.com>
2025-03-07 23:42:50 +0200

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-const express = require('express')
-require('./src/db/mongoose')
-//models
-const User = require('./src/models/user')
-const UserRouter = require('./src/routers/user')
+const express = require("express");
+require("./src/db/mongoose");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(UserRouter)
+app.use(express.json());
 
-app.listen(port,()=>{
-    console.log('server is up and listening on port, '+ port)
-})
+app.listen(port, () => {
+  console.log("server is up and listening on port, " + port);
+});
