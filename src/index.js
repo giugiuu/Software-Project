739c6cc73729691cb47789161f7bc6a962ed1f6a
amr <amr.hedeiwy@gmail.com>
2025-03-07 23:45:50 +0200

    Add .gitignore and package.json, update index.js configuration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 require("./src/db/mongoose");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log("server is up and listening on port, " + port);
+app.listen(PORT, () => {
+  console.log("Listening on PORT: " + PORT);
 });
