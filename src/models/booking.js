bf6522e496587dd1709c5d0b0f473eb0d2d9ec63
BahyHany <bahyhany063@gmail.com>
2025-03-07 23:33:22 +0200

    booking schema

---
+++ src/models/booking.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const bookingSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Event',
+    },
+    numberOfTickets: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'confirmed', 'canceled'],
+        default: 'pending',
+    },
+}, {
+    timestamps: true, 
+})
