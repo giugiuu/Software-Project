8aa69eed6a1bf5327a8aebec4da46b9f2b03f4a2
mohanad shehab <mohanadshehabb@gmail.com>
2025-03-07 23:17:42 +0200

    created event schema

---
+++ src/models/event.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true },
+    location: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model("Event", EventSchema);
+
+module.exports = Event;
+
