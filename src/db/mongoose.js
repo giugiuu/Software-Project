b7031f44d72ba2c36bd3e08bcd86f08d240b9fa6
amr <amr.hedeiwy@gmail.com>
2025-03-07 23:50:29 +0200

    Refactor database connection to use async function with error handling

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/event-project')
+const connectToDb = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/event-project");
+
+    console.log("mongodb successfully connected");
+  } catch (error) {
+    console.log("Error while connection to mongodb: ", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDb;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-require("./src/db/mongoose");
+const connectToDb = require("./db/mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectToDb();
   console.log("Listening on PORT: " + PORT);
 });
